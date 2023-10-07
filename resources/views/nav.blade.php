f00c0e5d3cef05b7abaea39bcb649df6db767831
Rajesh <rajesh@staplelogic.in>
2023-10-07 17:36:16 +0530

    Test

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -36,7 +36,8 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
   <a href="{{url('/')}}" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-    <h2 class="m-0 text-primary"><i class="fa fa-car me-3"></i>CarServ</h2>
+    <img class="img-fluid" src="{{ asset('public/img/logo.jpeg')}}" alt="" width="70" height="60">
+    <!-- <h2 class="m-0 text-primary"><i class="fa fa-car me-3"></i>CarServ</h2> -->
   </a>
   <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
   <span class="navbar-toggler-icon"></span>
