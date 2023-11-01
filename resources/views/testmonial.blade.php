9ff6e19ae02dc7f84fcc696b03c484c963d9a5d7
Rajesh <rajesh@staplelogic.in>
2023-11-01 16:53:49 +0530

    Add content

--- resources/views/testmonial.blade.php
+++ resources/views/testmonial.blade.php
@@ -7,32 +7,32 @@
             <div class="owl-carousel testimonial-carousel position-relative">
                 <div class="testimonial-item text-center">
                     <img class="bg-light rounded-circle p-2 mx-auto mb-3" src="{{ asset('public/img/testimonial-1.jpg')}}" style="width: 80px; height: 80px;">
-                    <h5 class="mb-0">Client Name</h5>
-                    <p>Profession</p>
+                    <h5 class="mb-0">Shashi Patial</h5>
+                    <p></p>
                     <div class="testimonial-text bg-light text-center p-4">
                     <p class="mb-0">It was a Wow! experience for me. The entire team was very curious from the start to the end. Pricing was competitive. I had to replace the front bumper as it was damaged. It was a neat finish.</p>
                     </div>
                 </div>
                 <div class="testimonial-item text-center">
                     <img class="bg-light rounded-circle p-2 mx-auto mb-3" src="{{ asset('public/img/testimonial-2.jpg')}}" style="width: 80px; height: 80px;">
-                    <h5 class="mb-0">Client Name</h5>
-                    <p>Profession</p>
+                    <h5 class="mb-0">Shiv Kumar Gupta</h5>
+                    <p></p>
                     <div class="testimonial-text bg-light text-center p-4">
                     <p class="mb-0">Car emergencies are not so bad when you have really good people to help. Clouds of billowing white smoke from my tailpipe were fixed promptly when I brought my car to the ever dependable folks.</p>
                     </div>
                 </div>
                 <div class="testimonial-item text-center">
                     <img class="bg-light rounded-circle p-2 mx-auto mb-3" src="{{ asset('public/img/testimonial-3.jpg')}}" style="width: 80px; height: 80px;">
-                    <h5 class="mb-0">Client Name</h5>
-                    <p>Profession</p>
+                    <h5 class="mb-0">Mukesh Chauhan</h5>
+                    <p></p>
                     <div class="testimonial-text bg-light text-center p-4">
                     <p class="mb-0">Very nice service. Friendly staff and seamless handling of any repair and service work for any type of car. I had given my car for a small fix and also servicing, polishing - all work done very nicely..</p>
                     </div>
                 </div>
                 <div class="testimonial-item text-center">
                     <img class="bg-light rounded-circle p-2 mx-auto mb-3" src="{{ asset('public/img/testimonial-4.jpg')}}" style="width: 80px; height: 80px;">
-                    <h5 class="mb-0">Client Name</h5>
-                    <p>Profession</p>
+                    <h5 class="mb-0">Rajesh Thakur</h5>
+                    <p></p>
                     <div class="testimonial-text bg-light text-center p-4">
                     <p class="mb-0">Wonderful car service by Aaryav Car Service. I used their comprehensive car service. Their service always comes with quality assurance. </p>
                     </div>
